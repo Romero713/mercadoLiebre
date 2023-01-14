df3800dc5759d76b1ecc14ee1671856fb059153e
Romero713 <mauricioromerr2012@gmail.com>
2023-01-14 17:10:23 -0300

    arreglando pequeños detalles al tp

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000
 app.use(express.static(path.resolve(__dirname,'public')))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','home.html')))
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views/login.html')))
+app.get('/register',(req,res) =>res.sendFile(path.resolve(__dirname,'views/register.html')))
 
 
 app.listen(port,() => console.log(`El servidor fue levantado con exito en http://localhost:${port}`))
